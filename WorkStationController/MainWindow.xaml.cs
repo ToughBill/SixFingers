10923ff10db156043708f8ccbe0887dd35646619
diaoyuanyi <diaoyuanyi@0e29bd18-3f2e-4170-b5bb-3fd49a12c5d5>
2014-09-05 02:52:18 +0000

    Draft of main GUI layout.

--- WorkStationController/MainWindow.xaml.cs
+++ WorkStationController/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CarvoEx2
+namespace WorkstationController
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
