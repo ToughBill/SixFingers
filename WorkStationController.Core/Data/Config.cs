b98d6a2d5b264b7447ae4a318631270403eef6f9
kingofcrabs <kingofcrabs@0e29bd18-3f2e-4170-b5bb-3fd49a12c5d5>
2014-09-10 22:22:29 +0000

    add configuration definition.

---
+++ WorkStationController.Core/Data/Config.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkstationController.Core.Data;
+
+namespace WorkStationController.Core.Data
+{
+    public class Configurations
+    {
+        static Configurations instance = null;
+        static public Configurations Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Configurations();
+                return instance;
+            }
+        }
+        private Configurations()
+        {
+
+        }
+
+        private List<ArmInfo> armsInfo;
+        private Worktable tableSurface;
+        public int pumpComPort;
+        
+        public List<ArmInfo> ArmsInfo
+        {
+            get
+            {
+                return armsInfo;
+            }
+            set
+            {
+                armsInfo = value;
+            }
+        }
+
+    }
+
+    public class ArmInfo
+    {
+        public byte ID;
+        public byte address;
+        private List<TipType> tipsType;
+        private List<PumpInfo> pumpsInfo;
+        public int TipCount 
+        { 
+            get
+            {
+                return tipsType.Count;
+            }
+        }
+
+        public List<TipType> TipsType
+        { 
+            get
+            {
+                return tipsType;
+            }
+            set
+            {
+                tipsType = value;
+            }
+        }
+
+        public List<PumpInfo> PumpsInfo
+        {
+            get
+            {
+                return pumpsInfo;
+            }
+            set
+            {
+                pumpsInfo = value;
+            }
+        }
+    }
+    public class TipInfo
+    {
+        public TipType tipType;
+        public double DilutorCapacity;
+    }
+
+    public enum TipType
+    {
+        Fixed = 0,
+        Disposable = 1,
+    }
+}
---
+++ WorkStationController.Core/Data/PumpInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WorkstationController.Core.Utility;
+
+namespace WorkstationController.Core.Data
+{
+    public class PumpInfo
+    {
+        public byte ID { get; set; }
+        public byte address { get; set; }
+        public int syringeVolumeUL { get; set; } 
+        public int totalMotorSteps { get; set; }
+    }
+}
