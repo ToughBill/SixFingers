d3d04d2d0077166f521b603b10cb6772307ff457
kingofcrabs <kingofcrabs@0e29bd18-3f2e-4170-b5bb-3fd49a12c5d5>
2014-09-10 21:56:42 +0000

    work table

---
+++ WorkStationController.Core/Data/Worktable.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using WorkstationController.Core.Utility;
+
+namespace WorkStationController.Core.Data
+{
+     public class Worktable : ISerialization
+    {
+        public Size worktableSize;
+        
+         // the size of each rows' pin, normally the first row's size is smaller
+        // but the second & third are same.
+        public Size firstRowPinSize;
+        public Size secondRowPinSize;
+        public Size thirdRowPinSize; 
+
+        //position of the left/top pin
+        public Point firstPinPosition;
+
+        //the pin's y position of the second row, there are 3 rows of pin
+        public double yPosRows2Pin;
+        public double yPosRows3Pin;
+
+        private Worktable()
+        {
+        }
+
+        //pin's color is defaulted to black
+        //worktable's color is default to lightGray
+        //public void Draw(System.Windows.Media.DrawingContext drawingContext, Size wholeWindow)
+        //{
+        //    throw new NotImplementedException();
+        //}
+        public static Worktable Create(string fromXmlFile)
+        {
+            return SerializationHelper.Deserialize<Worktable>(fromXmlFile);
+        }
+
+        public void Serialize(string toXmlFile)
+        {
+            SerializationHelper.Serialize<Worktable>(toXmlFile, this);
+        }
+    }
+}
