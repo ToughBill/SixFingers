64c43a7d984d8ec6746e1dfc12272a6cde6c306e
kingofcrabs <kingofcrabs@0e29bd18-3f2e-4170-b5bb-3fd49a12c5d5>
2014-09-14 10:05:53 +0000

    just test UI

--- TestWorktable/MainWindow.xaml.cs
+++ TestWorktable/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,14 +33,19 @@ namespace TestWorktable
         void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             Worktable worktable = new Worktable(
-                new Size(400, 800),
+                new Size(4000, 8000),
                 new Size(20, 40), 
                 new Size(20, 60),
-                new Size(20, 60), new Point(50, 50),150,250,20);
-            
-            WorktableVisual worktableVisual = new WorktableVisual(worktable,new Size(400,500));
-            canvas1.AttachWorktableVisual(worktableVisual);
-            canvas1.InvalidateVisual();
+                new Size(20, 60), new Point(50, 50),1500,3500,20);
+            Configurations.Instance.Worktable = worktable;
+            WorktableVisual worktableVisual = new WorktableVisual();
+            grid1.AttachWorktableVisual(worktableVisual);
+            grid1.InvalidateVisual();
         }
+
+        //private void grid1_SizeChanged(object sender, SizeChangedEventArgs e)
+        //{
+        //    Debug.WriteLine("size changed : {0} , {1}", e.NewSize.Width, e.NewSize.Height);
+        //}
     }
 }
---
+++ TestWorktable/MyGrid.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Visuals;
+
+namespace TestWorktable
+{
+    class MyGrid : Grid
+    {
+        WorktableVisual worktableVisual = null;
+        List<CarrierUIElement> carrierUIElements = new List<CarrierUIElement>();
+        public void AttachWorktableVisual(WorktableVisual worktableVisual)
+        {
+            this.worktableVisual = worktableVisual;
+            this.SizeChanged += MyCanvas_SizeChanged;
+        }
+
+        void MyCanvas_SizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
+        {
+            if (worktableVisual == null)
+                return;
+
+            foreach (CarrierUIElement carrierUI in carrierUIElements)
+                carrierUI.Update();
+
+            UpdateContainerSize(e.NewSize);
+            this.InvalidateVisual();
+        }
+
+        private void UpdateContainerSize(System.Windows.Size size)
+        {
+            VisualCommon.containerSize = size;
+        }
+
+        protected override void OnRender(System.Windows.Media.DrawingContext dc)
+        {
+            if( worktableVisual != null)
+                worktableVisual.Draw(dc);
+            base.OnRender(dc);
+        }
+    }
+}
