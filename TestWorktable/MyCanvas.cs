f7bd9e887f22b21e37490c01983c5d022fccab99
kingofcrabs <kingofcrabs@0e29bd18-3f2e-4170-b5bb-3fd49a12c5d5>
2014-09-14 03:12:04 +0000

    TestWorktable

---
+++ TestWorktable/MyCanvas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Visuals;
+
+namespace TestWorktable
+{
+    class MyCanvas : Canvas
+    {
+        WorktableVisual worktableVisual = null;
+
+        public void AttachWorktableVisual(WorktableVisual worktableVisual)
+        {
+            this.worktableVisual = worktableVisual;
+        }
+
+        protected override void OnRender(System.Windows.Media.DrawingContext dc)
+        {
+            if( worktableVisual != null)
+                worktableVisual.Draw(dc);
+            base.OnRender(dc);
+        }
+    }
+}
