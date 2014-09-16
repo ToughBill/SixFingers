1c5bdf4274d3bc35e54ce15da331ca859966bcb8
diaoyuanyi <diaoyuanyi@0e29bd18-3f2e-4170-b5bb-3fd49a12c5d5>
2014-09-16 06:30:15 +0000

    LiquidClass implements INotifyPropertyChanged for binding and ICloneable for duplication operation.

--- WorkStationController.Core/Data/LiquidClass.cs
+++ WorkStationController.Core/Data/LiquidClass.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Xml.Serialization;
 using WorkstationController.Core.Utility;
 
@@ -9,35 +10,41 @@ namespace WorkstationController.Core.Data
     /// Data definition of liquid class
     /// </summary>
     [Serializable]
-    public class LiquidClass : ISerialization
+    public class LiquidClass : ISerialization, INotifyPropertyChanged, ICloneable
     {
-        /// <summary>
-        /// Aspiration pipetting instance
-        /// </summary>
-        private AspirationPipetting aspirationSinglePipetting = new AspirationPipetting();
+        #region private members for properties
+        private string _name = "<Need a name>";      
+        private AspirationPipetting _aspirationSinglePipetting = new AspirationPipetting();
+        private DispensePipetting _dispenseSinglePipetting = new DispensePipetting();
+        #endregion
 
         /// <summary>
-        /// Dispense pipetting instance
+        /// Property changed event
         /// </summary>
-        private DispensePipetting dispenseSinglePipetting = new DispensePipetting();
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
 
         /// <summary>
         /// Gets or sets the name of liquid class
         /// </summary>
         [XmlAttribute]
-        public string Name { get; set; }
+        public string Name 
+        {
+            get { return this._name; }
+            set { PropertyChangedNotifyHelper.NotifyPropertyChanged<string>(ref this._name, value, this, "Name", this.PropertyChanged); }
+        }
 
         /// <summary>
         /// Gets the aspiration single pipetting parameters
         /// </summary>
         [XmlElement]
         public AspirationPipetting AspirationSinglePipetting 
-        { 
-            get { return this.aspirationSinglePipetting; }
+        {
+            get { return this._aspirationSinglePipetting; }
             set 
             {
-                this.aspirationSinglePipetting = null;
-                this.aspirationSinglePipetting = value; 
+                this._aspirationSinglePipetting = null;
+                this._aspirationSinglePipetting = value;
+                PropertyChangedNotifyHelper.NotifyPropertyChanged<AspirationPipetting>(ref this._aspirationSinglePipetting, value, this, "AspirationSinglePipetting", this.PropertyChanged);
             }
         }
 
@@ -47,11 +54,12 @@ namespace WorkstationController.Core.Data
         [XmlElement]
         public DispensePipetting DispenseSinglePipetting 
         {
-            get { return this.dispenseSinglePipetting; }
+            get { return this._dispenseSinglePipetting; }
             set 
             {
-                this.dispenseSinglePipetting = null;
-                this.dispenseSinglePipetting = value; 
+                this._dispenseSinglePipetting = null;
+                this._dispenseSinglePipetting = value;
+                PropertyChangedNotifyHelper.NotifyPropertyChanged<DispensePipetting>(ref this._dispenseSinglePipetting, value, this, "DispenseSinglePipetting", this.PropertyChanged);
             }
         }
 
@@ -79,55 +87,130 @@ namespace WorkstationController.Core.Data
         {
             SerializationHelper.Serialize<LiquidClass>(toXmlFile, this);
         }
+
+        /// <summary>
+        /// Clone a new instance of AspirationPipetting with same value as current instance
+        /// </summary>
+        /// <returns>New instance of AspirationPipetting with same value as current instance</returns>
+        public object Clone()
+        {
+            LiquidClass copy = new LiquidClass();
+
+            copy._aspirationSinglePipetting = (AspirationPipetting)this._aspirationSinglePipetting.Clone();
+            copy._dispenseSinglePipetting = (DispensePipetting)this._dispenseSinglePipetting.Clone();
+
+            return copy;
+        }
     }
 
     /// <summary>
     /// Aspiration pipetting parameters
     /// </summary>
     [Serializable]
-    public class AspirationPipetting : ISerialization
+    public class AspirationPipetting : ISerialization, INotifyPropertyChanged, ICloneable
     {
+        #region private members for properties
+        private int _aspirationSpeed = 0;
+        private int _delay = 0;
+        private int _systemTrailingAirgap = 0;
+        private int _leadingAirgap = 0;
+        private int _trailingAirgap = 0;
+        private int _excessVolume = 0;
+        private int _conditioningVolume = 0;
+        #endregion
+
+        /// <summary>
+        /// Property changed event
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
         /// <summary>
         /// Gets or sets the aspiration speed in unit of μl / s
         /// </summary>
         [XmlElement]
-        public int AspirationSpeed { get; set; }
+        public int AspirationSpeed 
+        {
+            get { return this._aspirationSpeed; }
+            set { PropertyChangedNotifyHelper.NotifyPropertyChanged<int>(ref this._aspirationSpeed, value, this, "AspirationSpeed", this.PropertyChanged); }
+        }
 
         /// <summary>
         /// Gets or sets the delay time in unit of ms
         /// </summary>
         [XmlElement]
-        public int Delay { get; set; }
+        public int Delay 
+        {
+            get { return this._delay; }
+            set
+            {
+                PropertyChangedNotifyHelper.NotifyPropertyChanged<int>(ref this._delay, value, this, "Delay", this.PropertyChanged);
+            } 
+        }
 
         /// <summary>
         /// Gets or sets the system trailing airgap in unit of μl
         /// </summary>
         [XmlElement]
-        public int SystemTrailingAirgap { get; set; }
+        public int SystemTrailingAirgap
+        {
+            get { return this._systemTrailingAirgap; }
+            set
+            {
+                PropertyChangedNotifyHelper.NotifyPropertyChanged<int>(ref this._systemTrailingAirgap, value, this, "SystemTrailingAirgap", this.PropertyChanged);
+            }
+        }
 
         /// <summary>
         /// Gets or sets the leading airgap in unit of μl
         /// </summary>
         [XmlElement]
-        public int LeadingAirgap { get; set; }
+        public int LeadingAirgap
+        {
+            get { return this._leadingAirgap; }
+            set
+            {
+                PropertyChangedNotifyHelper.NotifyPropertyChanged<int>(ref this._leadingAirgap, value, this, "LeadingAirgap", this.PropertyChanged);
+            }
+        }
 
         /// <summary>
         /// Gets or sets the leading airgap in unit of μl
         /// </summary>
         [XmlElement]
-        public int TrailingAirgap { get; set; }
+        public int TrailingAirgap
+        {
+            get { return this._trailingAirgap; }
+            set
+            {
+                PropertyChangedNotifyHelper.NotifyPropertyChanged<int>(ref this._trailingAirgap, value, this, "TrailingAirgap", this.PropertyChanged);
+            }
+        }
 
         /// <summary>
         /// Gets or sets the excess volume in unit of μl
         /// </summary>
         [XmlElement]
-        public int ExcessVolume { get; set; }
+        public int ExcessVolume
+        {
+            get { return this._excessVolume; }
+            set
+            {
+                PropertyChangedNotifyHelper.NotifyPropertyChanged<int>(ref this._excessVolume, value, this, "ExcessVolume", this.PropertyChanged);
+            }
+        }
 
         /// <summary>
         /// Gets or sets the conditioning volume in unit of μl
         /// </summary>
         [XmlElement]
-        public int ConditioningVolume { get; set; }
+        public int ConditioningVolume
+        {
+            get { return this._conditioningVolume; }
+            set
+            {
+                PropertyChangedNotifyHelper.NotifyPropertyChanged<int>(ref this._conditioningVolume, value, this, "ConditioningVolume", this.PropertyChanged);
+            }
+        }
 
         /// <summary>
         /// Default constructor
@@ -153,31 +236,82 @@ namespace WorkstationController.Core.Data
         {
             SerializationHelper.Serialize<AspirationPipetting>(toXmlFile, this);
         }
+
+        /// <summary>
+        /// Clone a new instance of AspirationPipetting with same value as current instance
+        /// </summary>
+        /// <returns>New instance of AspirationPipetting with same value as current instance</returns>
+        public object Clone()
+        {
+            AspirationPipetting copy = new AspirationPipetting();
+
+            copy._aspirationSpeed = this._aspirationSpeed;
+            copy._delay = this._delay;
+            copy._systemTrailingAirgap = this._systemTrailingAirgap;
+            copy._leadingAirgap = this._leadingAirgap;
+            copy._trailingAirgap = this._trailingAirgap;
+            copy._excessVolume = this._excessVolume;
+            copy._conditioningVolume = this._conditioningVolume;
+
+            return copy;
+        }
     }
 
     /// <summary>
     /// Dispense pipetting parameters
     /// </summary>
     [Serializable]
-    public class DispensePipetting : ISerialization
+    public class DispensePipetting : ISerialization, INotifyPropertyChanged, ICloneable
     {
+        #region private members for properties
+        private int _dispenseSpeed = 0;
+        private int _delay = 0;
+        private bool _trailingAirgapAfterDispense = false;
+        #endregion
+
+        /// <summary>
+        /// Property changed event
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
         /// <summary>
         /// Gets or sets the dispense speed in unit of μl / s
         /// </summary>
         [XmlElement]
-        public int DispenseSpeed { get; set; }
+        public int DispenseSpeed
+        {
+            get { return this._dispenseSpeed; }
+            set
+            {
+                PropertyChangedNotifyHelper.NotifyPropertyChanged<int>(ref this._dispenseSpeed, value, this, "DispenseSpeed", this.PropertyChanged);
+            }
+        }
 
         /// <summary>
         /// Gets or sets the delay time in unit of ms
         /// </summary>
         [XmlElement]
-        public int Delay { get; set; }
+        public int Delay
+        {
+            get { return this._delay; }
+            set
+            {
+                PropertyChangedNotifyHelper.NotifyPropertyChanged<int>(ref this._delay, value, this, "Delay", this.PropertyChanged);
+            }
+        }
 
         /// <summary>
         /// Gets or sets whether trailing airgap after each dispense
         /// </summary>
         [XmlElement]
-        public bool TrailingAirgapAfterDispense { get; set; }
+        public bool TrailingAirgapAfterDispense
+        {
+            get { return this._trailingAirgapAfterDispense; }
+            set
+            {
+                PropertyChangedNotifyHelper.NotifyPropertyChanged<bool>(ref this._trailingAirgapAfterDispense, value, this, "TrailingAirgapAfterDispense", this.PropertyChanged);
+            }
+        }
 
         /// <summary>
         /// Default constructor
@@ -202,6 +336,21 @@ namespace WorkstationController.Core.Data
         public void Serialize(string toXmlFile)
         {
             SerializationHelper.Serialize<DispensePipetting>(toXmlFile, this);
+        }
+
+        /// <summary>
+        /// Clone a new instance of DispensePipetting with same value as current instance
+        /// </summary>
+        /// <returns>New instance of DispensePipetting with same value as current instance</returns>
+        public object Clone()
+        {
+            DispensePipetting copy = new DispensePipetting();
+
+            copy._dispenseSpeed = this._dispenseSpeed;
+            copy._delay = this._delay;
+            copy._trailingAirgapAfterDispense = this._trailingAirgapAfterDispense;
+
+            return copy;
         }
     }
 }
---
+++ WorkStationController.Core/Utility/PropertyChangedNotifyHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace WorkstationController.Core.Utility
+{
+    /// <summary>
+    /// Static helper class for notifying object property change
+    /// </summary>
+    public static class PropertyChangedNotifyHelper
+    {
+        public static void NotifyPropertyChanged<T>(ref T oldValue, T setValue, object sender, string propertyName, PropertyChangedEventHandler handler)
+        {
+            if (oldValue.Equals(setValue))
+                return;
+
+            oldValue = setValue;
+
+#if DEBUG
+            // Output debug message for notifying value changed
+            string debug_message = string.Format("[{0}] changed - Old Value: {1}; New Value: {2}",
+                propertyName, oldValue, setValue);
+            Trace.WriteLine(debug_message);
+#endif
+
+            handler(sender, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
