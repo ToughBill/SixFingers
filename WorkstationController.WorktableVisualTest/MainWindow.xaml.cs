3d5b87d9cf8aac81709b49403d2a69fe48889d90
kingofcrabs <kingofcrabs@0e29bd18-3f2e-4170-b5bb-3fd49a12c5d5>
2014-10-21 23:58:43 +0000

    for test the new labwareUIElementFixedSize

--- WorkstationController.WorktableVisualTest/MainWindow.xaml.cs
+++ WorkstationController.WorktableVisualTest/MainWindow.xaml.cs
@@ -32,61 +32,26 @@ namespace WorkstationController.WorktableVisualTest
 
         void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            Worktable worktable = new Worktable(
-                new Size(6000, 4000),
-                new Size(20, 80),
-                new Size(20, 120),
-                new Size(20, 120), new Point(500, 1000), 1800, 3000, 16);
-            Configurations.Instance.Worktable = worktable;
+            Labware labware1 = new Labware();
+            labware1.ZValues = new ZValues(360, 625, 665, 1610);
+            labware1.Dimension = new Dimension(1270,855);
+            labware1.TypeName = "16Pos Tubes";
+            labware1.Label = "lab1";
+            labware1.SiteID = 1;
+            labware1.WellsInfo = new WellsInfo(new Point(144, 115), new Point(990+144, 745), 12, 8, BottomShape.Flat, 32);
+            labware1.TypeName = LabwareBuildInType.Plate96_05ML.ToString();
+            labware1.BackgroundColor = Color.FromArgb(255, 255, 0, 0);
+            labware1.CarrierGrid = 0;
 
-            Container.SizeChanged += Container_SizeChanged;
-            WorktableVisual worktableVisual = new WorktableVisual();
-            grid1.AttachWorktableVisual(worktableVisual);
-            //grid1.InvalidateVisual();
-            //Labware labware = new Labware();
-            //labware.ZValues = new ZValues(360, 625, 665, 1610);
-            //labware.Dimension = new Dimension(250, 2800);
-            //labware.Name = "lab1";
-            //labware.SiteID = 1;
-            //labware.Type = LabwareType.Microplates;
-            //labware.WellsInfo = new WellsInfo(new Point(0, -32), new Point(0, 2788), 1, 16, BottomShape.Flat, 50);
-            //labware.Type = LabwareType.Tubes;
-            //labware.BackGroundColor = Color.FromArgb(255, 120, 0, 0);
-            //labware.CarrierLabel = "";
-            //Container.Children.Add(new LabwareUIElement(labware));
-            
-            
-            //Labware labware1 = new Labware();
-            //labware1.ZValues = new ZValues(360, 625, 665, 1610);
-            //labware1.Dimension = new Dimension(250, 2700);
-            //labware1.TypeName = "16Pos Tubes";
-            //labware1.Label = "lab1";
-            //labware1.SiteID = 1;
-            //labware1.WellsInfo = new WellsInfo(new Point(0, -32), new Point(0, 2788), 1, 16, BottomShape.Flat, 50);
-            //labware1.TypeName = "Tubes 16Pos 100mm";
-            //labware1.BackGroundColor = Color.FromArgb(255, 255, 0, 0);
-            //labware1.CarrierLabel = "";
-            Carrier carrier1 = new Carrier(BuildInCarrierType.MP_3POS);
-            carrier1.Label = "Carrier1";
-            var carrierUIElement = new CarrierUIElement(carrier1);
-            carrierUIElement.Selected = true;
+            LabwareUIElementFixedSize labwareUIElement = new LabwareUIElementFixedSize(labware1, new Size(400, 400));
+            grid1.Children.Add(labwareUIElement);
 
-            Container.Children.Add(carrierUIElement);
-            Container.RenderSize = new Size(600, 800);
-            VisualCommon.UpdateVisuals(Container.Children);
+            this.MouseMove += MainWindow_MouseMove;
         }
 
-      
-
-        void Container_SizeChanged(object sender, SizeChangedEventArgs e)
+        void MainWindow_MouseMove(object sender, MouseEventArgs e)
         {
-            VisualCommon.UpdateContainerSize(e.NewSize);
-            VisualCommon.UpdateVisuals(Container.Children);
-        }
-
-        private void btnNewLabware_Click(object sender, RoutedEventArgs e)
-        {
-
+            grid1.InvalidateVisual();
         }
     }
 }
--- WorkstationController.WorktableVisualTest/WorktableGrid.cs
+++ WorkstationController.WorktableVisualTest/WorktableGrid.cs
@@ -1,14 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 using WorkstationController.VisualElement;
 
 
 namespace WorkstationController.WorktableVisualTest
 {
+    public class WorktableGrid : Grid
+    {
+        protected override void OnRender(System.Windows.Media.DrawingContext dc)
+        {
+            base.OnRender(dc);
+            double x = Mouse.GetPosition(this).X;
+            double y = Mouse.GetPosition(this).Y;
+            dc.DrawText(new FormattedText(string.Format("x:{0} y:{1}", x, y),
+                       CultureInfo.GetCultureInfo("en-us"),
+                       FlowDirection.LeftToRight,
+                       new Typeface("Verdana"),
+                       15, System.Windows.Media.Brushes.DarkBlue),
+                       new System.Windows.Point(ActualWidth - 150, ActualHeight - 100));
 
+        }
+    }
 }
