e144a45a0b02175e05f6b5363358d900872036c8
kingofcrabs <kingofcrabs@0e29bd18-3f2e-4170-b5bb-3fd49a12c5d5>
2014-10-21 23:57:57 +0000

    fixed size UIElement is for selecting wells in pipetting commmands. We will not use the old LabwareUIElement implementation because it would be too complex for hitTest,highlight etc.

--- WorkstationController.Visual/LabwareUIElementFixedSize.cs
+++ WorkstationController.Visual/LabwareUIElementFixedSize.cs
@@ -8,7 +8,7 @@ using WorkstationController.Core.Data;
 
 namespace WorkstationController.VisualElement
 {
-    class LabwareUIElementFixedSize : UIElement
+    public class LabwareUIElementFixedSize : UIElement
     {
 
         /// <summary>
@@ -18,15 +18,18 @@ namespace WorkstationController.VisualElement
 
         private VisualCollection _children;
         private Size _boudingSize;
+        
         /// <summary>
         /// ctor
         /// </summary>
         /// <param name="labware"></param>
+        /// <param name="boundingSize"></param>
         public LabwareUIElementFixedSize(Labware labware,Size boundingSize)
         {
             this._labware = labware;
-            _children.Add(CreateBorderVisual());
             UpdateContainerSize(boundingSize);
+            _children = new VisualCollection(this);
+            _children.Add(CreateBorderVisual());
             AddWellVisuals();
         }
 
@@ -49,14 +52,47 @@ namespace WorkstationController.VisualElement
         {
             int cols = _labware.WellsInfo.NumberOfWellsX;
             int rows = _labware.WellsInfo.NumberOfWellsY;
+            int wellIndex = 0;
             for (int row = 0; row < rows; row++)
             {
                 for (int col = 0; col < cols; col++)
                 {
                     var position = _labware.GetPosition(row, col);
-                    //VisualCommon.DrawCircle(position, _labware.WellsInfo.WellRadius, drawingContext, _labware.BackgroundColor);
+                    _children.Add(CreateWellVisual(position, wellIndex++));
                 }
             }
+        }
+
+        private MyDrawingVisual CreateWellVisual(Point position, int index)
+        {
+            MyDrawingVisual drawingVisual = new MyDrawingVisual();
+            DrawingContext drawingContext = drawingVisual.RenderOpen();
+            int radius = _labware.WellsInfo.WellRadius;
+            Brush brush = Brushes.White;
+            Size labwareSize = new Size(_labware.Dimension.XLength,_labware.Dimension.YLength);
+            Point ptVisual = Physical2Visual(position, labwareSize, _boudingSize);
+            Size sz = new Size(radius, radius);
+            sz = Physical2Visual(sz, labwareSize, _boudingSize);
+            drawingContext.DrawEllipse(brush, new Pen(new SolidColorBrush(Colors.Black), 1), ptVisual, sz.Width, sz.Height);
+            drawingVisual.Index =  index;
+            drawingContext.Close();
+            return drawingVisual;
+        }
+
+        private Size Physical2Visual(Size sz, Size labwareSize, Size _boudingSize)
+        {
+            Size szVisual = new Size();
+            szVisual.Width = sz.Width / labwareSize.Width * _boudingSize.Width;
+            szVisual.Height = sz.Height / labwareSize.Height * _boudingSize.Height;
+            return szVisual;
+        }
+
+        private Point Physical2Visual(Point position, Size labwareSize, Size _boudingSize)
+        {
+            Point ptVisual = new Point();
+            ptVisual.X = position.X / labwareSize.Width * _boudingSize.Width;
+            ptVisual.Y = position.Y / labwareSize.Height * _boudingSize.Height;
+            return ptVisual;
         }
 
 
@@ -74,13 +110,14 @@ namespace WorkstationController.VisualElement
 
         private void DrawRect(Rect Rectangle, Color color, DrawingContext drawingContext)
         {
-            Brush brush = new SolidColorBrush(color);
-            drawingContext.DrawRectangle(brush, new Pen(new SolidColorBrush(Colors.Black), 1), Rectangle);
+            drawingContext.DrawRectangle(null, new Pen(new SolidColorBrush(color), 1), Rectangle);
         }
 
         internal void DrawCircle(Point ptCenter,double radius, Color color, DrawingContext drawingContext)
         {
             Brush brush = new SolidColorBrush(color);
+            if (color == Colors.White)
+                brush = null;
             drawingContext.DrawEllipse(brush, new Pen(new SolidColorBrush(Colors.Black), 1), ptCenter, radius, radius);
         }
 
