308778f04e52a973e235dbc09978264e2805357a
kingofcrabs <kingofcrabs@0e29bd18-3f2e-4170-b5bb-3fd49a12c5d5>
2014-10-31 00:21:15 +0000

    refine uielement

--- WorkstationController.EditorTests/LayoutEditorTestharness.xaml.cs
+++ WorkstationController.EditorTests/LayoutEditorTestharness.xaml.cs
@@ -58,17 +58,28 @@ namespace WorkstationController.EditorTests
             Labware labware2 = new Labware();
             labware2.ZValues = new ZValues(360, 625, 665, 1610);
             labware2.Dimension = new Dimension(1270, 855);
-            labware2.Label = "lab1";
+            labware2.Label = "lab2";
             labware2.SiteID = 1;
             labware2.WellsInfo = new WellsInfo(new Point(24, 29), new Point(1014, 609), 12, 8, BottomShape.Flat, 33);
             labware2.TypeName = LabwareBuildInType.Plate96_05ML.ToString();
             labware2.BackgroundColor = Color.FromArgb(255, 255, 0, 0);
             labware2.ParentCarrier = null;
 
+            Labware labware3 = new Labware();
+            labware3.ZValues = new ZValues(360, 625, 665, 1610);
+            labware3.Dimension = new Dimension(1270, 855);
+            labware3.Label = "lab3";
+            labware3.SiteID = 1;
+            labware3.WellsInfo = new WellsInfo(new Point(24, 29), new Point(1014, 609), 6, 4, BottomShape.Flat, 60);
+            labware3.TypeName = LabwareBuildInType.Plate24_2ML.ToString();
+            labware3.BackgroundColor = Color.FromArgb(255, 100,255, 0);
+            labware3.ParentCarrier = null;
+
             Carrier carrier1 = new Carrier(BuildInCarrierType.MP_3POS);
             Carrier carrier2 = new Carrier(BuildInCarrierType.Tube13mm_16POS);
             wares.Add(labware1);
             wares.Add(labware2);
+            wares.Add(labware3);
             wares.Add(carrier1);
             wares.Add(carrier2);
             lstboxLabwares.ItemsSource = wares;
