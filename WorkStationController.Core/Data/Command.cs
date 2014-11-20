573a45378afec2d3d99bd267780d01e59d80dd4c
diaoyuanyi <diaoyuanyi@0e29bd18-3f2e-4170-b5bb-3fd49a12c5d5>
2014-11-20 09:58:15 +0000

    Add Command and Script definition.

---
+++ WorkStationController.Core/Data/Command.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace WorkstationController.Core.Data
+{
+    /// <summary>
+    /// Command definition
+    /// </summary>
+    [Serializable]
+    public class Command : ICloneable
+    {
+        private string _name       = string.Empty;
+        private string _parameters = string.Empty;
+
+        /// <summary>
+        /// Default constructor for deserialization
+        /// </summary>
+        public Command()
+        { }
+
+        public Command(string name, string parameters)
+        {
+            this._name = name;
+            this._parameters = parameters;
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the command
+        /// </summary>
+        public string Name
+        {
+            get { return this._name; }
+            set { this._name = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the parameters of the command
+        /// </summary>
+        public string Parameters
+        {
+            get { return this._parameters; }
+            set { this._parameters = value; }
+        }
+
+        public object Clone()
+        {
+            return new Command(this._name, this._parameters);
+        }
+    }
+}
---
+++ WorkStationController.Core/Data/Script.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace WorkstationController.Core.Data
+{
+    /// <summary>
+    /// Script definition, that is a serise of Command instances.
+    /// </summary>
+    [Serializable]
+    public class Script : ObservableCollection<Command>
+    {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public Script() 
+        { }
+
+        /// <summary>
+        /// Inserts an command into the script at the specified index
+        /// </summary>
+        /// <param name="index">The zero-based index at which command should be inserted.</param>
+        /// <param name="command">The command to insert</param>
+        public void InsertCommand(int index, Command command)
+        {
+            this.Insert(index, command);
+        }
+
+        /// <summary>
+        /// Moves the command at the specified index to a new location
+        /// </summary>
+        /// <param name="oldIndex">The zero-based index specifying the location of the command to be moved.</param>
+        /// <param name="newIdex">The zero-based index specifying the new location of the command.</param>
+        public void MoveCommand(int oldIndex, int newIdex)
+        {
+            this.MoveItem(oldIndex, newIdex);
+        }
+
+        /// <summary>
+        /// Delete a command at the specified index
+        /// </summary>
+        /// <param name="index">The zero-based index of the command to remove.</param>
+        public void DeleteCommand(int index)
+        {
+            this.RemoveItem(index);
+        }
+    }
+}
