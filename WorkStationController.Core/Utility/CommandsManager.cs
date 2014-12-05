10eafcf6820336b5763d7f48ce6f66bd10222679
kingofcrabs <rex.xie@qq.com>
2014-12-06 07:15:57 +0800

    +

---
+++ WorkStationController.Core/Utility/CommandsManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WorkstationController.Core.Data
+{
+    class CommandsManager
+    {
+
+        private static CommandsManager _instance = null;
+        /// <summary>
+        /// Gets the single insntace of CommandsManager
+        /// </summary>
+        public static CommandsManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new CommandsManager();
+                }
+
+                return _instance;
+            }
+        }
+
+
+
+    }
+}
---
+++ WorkStationController.Core/Utility/FolderHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using WorkstationController.Core.Properties;
+
+namespace WorkstationController.Core.Utility
+{
+    public class FolderHelper
+    {
+        static public string GetExeFolder()
+        {
+            return Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+        }
+
+        static public string GetLayoutFolder()
+        {
+            string sFilePath =  Path.Combine(GetExeFolder(), Resources.LayoutFolder);
+            if (!Directory.Exists(sFilePath))
+                Directory.CreateDirectory(sFilePath);
+            return sFilePath;
+        }
+    }
+}
