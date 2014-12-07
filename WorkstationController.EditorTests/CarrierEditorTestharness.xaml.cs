f9eb23e4ed0acc8b67ed416f99af1a3eab4931d4
kingofcrabs <rex.xie@qq.com>
2014-12-08 07:20:56 +0800

    +

---
+++ WorkstationController.EditorTests/CarrierEditorTestharness.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WorkstationController.Control;
+
+namespace WorkstationController.EditorTests
+{
+    /// <summary>
+    /// Interaction logic for CarrierEditorTestharness.xaml
+    /// </summary>
+    public partial class CarrierEditorTestharness : Window
+    {
+        CarrierEditor carrierEditor;
+        public CarrierEditorTestharness()
+        {
+            InitializeComponent();
+            this.Loaded += new RoutedEventHandler(CarrierEditorTestharness_Loaded);
+        }
+
+        void CarrierEditorTestharness_Loaded(object sender, RoutedEventArgs e)
+        {
+            carrierEditor = new CarrierEditor(null);
+            container.Children.Add(carrierEditor);
+        }
+    }
+}
