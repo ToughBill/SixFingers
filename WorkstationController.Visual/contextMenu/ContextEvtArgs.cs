9117ebe8663c19732d79bbe2a25cb63d9284d878
kingofcrabs <rex.xie@qq.com>
2014-12-09 07:33:32 +0800

    context menu

---
+++ WorkstationController.Visual/contextMenu/ContextEvtArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace WorkstationController.VisualElement.ContextMenu
+{
+    /// <summary>
+    /// clicked ware
+    /// </summary>
+    class ContextEvtArgs : EventArgs
+    {
+        public BasewareUIElement Element { get; set; }
+        public Point Position { get; set; }
+        public bool Need2Show { get; set; }
+        public ContextEvtArgs(BasewareUIElement baseUIElement,Point position, bool need2Show = true)
+        {
+            Element = baseUIElement;
+            Position = position;
+            Need2Show = need2Show;
+        }
+    }
+}
---
+++ WorkstationController.Visual/contextMenu/contextMenuController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WorkstationController.VisualElement.Uitility;
+
+namespace WorkstationController.VisualElement.ContextMenu
+{
+    /// <summary>
+    /// process context menu of labware
+    /// </summary>
+    public class WareContextMenuController
+    {
+        ContextMenuForm contextMenuForm = new ContextMenuForm();
+        ContextWindowState windowState = ContextWindowState.closed;
+        /// <summary>
+        /// controller of ware's context menu.
+        /// </summary>
+        /// <param name="movementsController"></param>
+        public WareContextMenuController(UIMovementsController movementsController)
+        {
+            movementsController.onWareContextMenuFired += movementsController_onWareContextFired;
+        }
+
+        void movementsController_onWareContextFired(object sender, EventArgs e)
+        {
+            ContextEvtArgs contextEvtArgs = e as ContextEvtArgs;
+            if (!contextEvtArgs.Need2Show)
+            {
+                contextMenuForm.Hide();
+                windowState = ContextWindowState.closed;
+                return;
+            }
+
+            contextMenuForm.WindowStartupLocation = System.Windows.WindowStartupLocation.Manual;
+            contextMenuForm.Topmost = true;
+            if( windowState == ContextWindowState.closed)
+                contextMenuForm.Show();
+            else
+                windowState = ContextWindowState.show;
+            contextMenuForm.Left = contextEvtArgs.Position.X;
+            contextMenuForm.Top = contextEvtArgs.Position.Y;
+        }
+
+
+        enum ContextWindowState
+        {
+            closed = 0,
+            show
+        }
+    }
+}
