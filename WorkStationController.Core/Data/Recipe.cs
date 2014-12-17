ef3c8ab9cc9b9bea9e9dda6238799fa4ebc54c17
kingofcrabs <rex.xie@qq.com>
2014-12-17 10:30:07 +0800

    +

---
+++ WorkStationController.Core/Data/Recipe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WorkstationController.Core.Utility;
+
+namespace WorkstationController.Core.Data
+{
+    /// <summary>
+    /// recipe describes a test of biology
+    /// </summary>
+    public class Recipe : Layout, ISerialization
+    {
+        /// <summary>
+        /// scripts to be executed
+        /// </summary>
+        public List<string> Scripts { get; set; }
+
+
+
+
+        void ISerialization.Serialize(string toXmlFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
